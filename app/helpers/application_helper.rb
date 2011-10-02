3fad47bd5c8020e9907468385f4b4253ccd0e16e
Jefferson Carley <jeff.carley@gmail.com>
2011-10-02 10:03:21 -0500

    Added site navigation links.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  
+  # Return a title on a per-page basis
+  def title
+    base_title = "Ruby on Rails Tutorial Sample App"
+    
+    # an instance variable could be nil, and if its a string
+    #  could be empty.  i like to check for both here
+    if @title.nil? || @title.to_s.empty?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
+
 end
