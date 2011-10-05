84522251101399c82bcf849a9151b247a3ad3d08
Jefferson Carley <jeff.carley@gmail.com>
2011-10-05 18:29:45 -0500

    Finished first cut of the User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 SampleApp::Application.routes.draw do
 
-  get "demo/index"
-
   root :to => "pages#home"
+
+  resources :users  
   
   match '/contact', :to => "pages#contact"
   match '/about', :to => "pages#about"
   match '/help', :to => "pages#help"
   match '/signup', :to => "users#new"
+
+  get "demo/index"  
   
 end
