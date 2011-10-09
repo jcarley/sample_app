fdaee8ebed1640c6646e46561595187d6ee9af94
Jefferson Carley <jeff.carley@gmail.com>
2011-10-09 09:02:32 -0500

    Finished the password machinery

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -109,11 +109,56 @@ describe User do
         @user.should respond_to(:encrypted_password)
       end
       
+      it "should set the encrypted password" do
+        @user.encrypted_password.should_not be_blank
+      end
+      
+      it "should have a salt" do
+        !@user.should respond_to(:salt)
+      end
+      
+      describe "has_password? method" do
+        
+        it "should exit" do
+          @user.should respond_to(:has_password?)
+        end
+        
+        it "should return true if the passwords match" do
+          @user.has_password?(@attr[:password]).should be_true
+        end
+        
+        it "should return false if the passwords don't match" do
+          @user.has_password?("invalid").should be_false          
+        end
+        
+      end
+      
+      describe "authenticate method" do
+        
+        it "should exist" do
+          User.should respond_to(:authenticate)
+        end
+        
+        it "should return nil on email/password mismatch" do
+          User.authenticate(@attr[:email], "wrong_pass").should be_nil
+        end
+        
+        it "should return nil for an email address with no user" do
+          User.authenticate("bar@foo.com", @attr[:password]).should be_nil
+        end
+        
+        it "should return the user on email/password match" do
+          User.authenticate(@attr[:email], @attr[:password]).should == @user
+        end
+        
+      end
+      
     end
     
   end
   
 end
+
 
 
 # == Schema Information
@@ -126,5 +171,6 @@ end
 #  created_at         :datetime
 #  updated_at         :datetime
 #  encrypted_password :string(255)
+#  salt               :string(255)
 #
 
